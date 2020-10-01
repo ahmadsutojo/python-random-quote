c9bee0f09c3ee6212082c624bcbf361434151a40
ahmadsutojo <72114941+ahmadsutojo@users.noreply.github.com>
2020-10-01 11:11:20 +0700

    Update get-quote.py
    add more fiture

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
+def panjang_file(fname):
+        with open(fname) as f:
+                for i, l in enumerate(f):
+                        pass
+        return i + 1
+
 def main():
-  # print("Keep it logically awesome.")
+  print("Number of lines in the file: ", panjang_file("quotes.txt"))
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  print(quotes)
 
 if __name__== "__main__":
   main()
